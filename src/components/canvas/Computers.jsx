ae79c6a816b2278d3c9866cd9e6f4e1aa4daf9c9
Anurag Meena <anurag31415@gmail.com>
2023-05-28 23:38:54 +0530

    import required components for Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const computers = useGLTF('./desktop_pc/scene.gltf')
+
   return (
     <div>Computers</div>
   )

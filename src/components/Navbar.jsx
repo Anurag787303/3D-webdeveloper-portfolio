1ab2561911aa730c5fff6f93168b84808bcc1b83
Anurag Meena <anurag31415@gmail.com>
2023-05-28 11:14:12 +0530

    make the name in the top to appear on same line

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,9 +25,11 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">Anurag <span
-            className="sm:block hidden">|
-            Javascript Mastery</span></p>
+          <p className="text-white text-[18px] 
+          font-bold cursor-pointer flex">
+            Anurag &nbsp;
+            <span className="sm:block hidden">|
+              Javascript Mastery</span></p>
         </Link>
 
         <ul className="list-none hidden sm:flex flex-row gap-10">
